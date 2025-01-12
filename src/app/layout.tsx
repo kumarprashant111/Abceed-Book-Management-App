c668dbdaf47b2b6f2d838e7988a6e40cea2a2eca
kumarprashant111 <kumar.prashant@qenest.com>
2025-01-12 21:00:56 +0900

    Added dark mode css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "@/components/atoms/Loading";
 import { Noto_Sans_JP } from "next/font/google";
 import { AppProvider } from "@/context/AppContext";
@@ -16,8 +16,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    // Check OS-level color scheme preference
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    setTheme(prefersDarkMode ? "dark" : "light");
+
+    // Listen for changes in the color scheme
+    const handleThemeChange = (e: MediaQueryListEvent) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", handleThemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleThemeChange);
+    };
+  }, []);
+
   return (
-    <html lang="ja">
+    <html lang="ja" className={theme}>
       <body className={notoSansJP.className}>
         <AppProvider>
           <main>
